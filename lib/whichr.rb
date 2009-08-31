132c15d8ec9e1d480907311830219ccfcbedc6d2
rogerdpack <rogerpack2005@gmail.com>
2009-08-31 07:04:06 -0600

    librar-ify

---
+++ lib/whichr.rb
@@ -0,0 +1,35 @@
+# stolen from the gnuplot gem
+# and then modified
+require 'rubygems'
+require 'sane'
+
+class RubyWhich
+  # search the path for the given names
+  # like ['abc'] (in windows, also searches for abc.bat)
+  # or ['ab*'] (a glob, in windows, also reveals ab*.bat)
+  def which( names )
+
+    if RUBY_PLATFORM =~ /mswin|mingw/
+      for name in names.dup # avoid recursion
+        # windows compat.
+        for extension in  ENV['PATHEXT'].split(';') do
+          names << name + extension
+        end
+      end
+    end
+
+    all_found = []
+    path = ENV['PATH']
+    # on windows add . [the cwd]
+    path += ('.' + File::PATH_SEPARATOR) if RUBY_PLATFORM =~ /mswin|mingw/
+
+    path.split(File::PATH_SEPARATOR).each do |dir|
+      for name in names
+        all_found += Dir.glob(dir.gsub("\\", "/") + '/' + name.strip)
+      end
+    end
+
+    all_found
+  end
+
+end
--- whichr.gemspec
+++ whichr.gemspec
@@ -1,15 +1,15 @@
 Gem::Specification.new do |s|
   s.name = %q{whichr}
-  s.version = "0.1.3"
+  s.version = "0.1.4"
  
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Roger Pack"]
   s.description = s.summary = %q{windows friendly which command}
   s.email = ["rogerdpack@gmail.comm"]
-  s.files = ["bin/whichr", "README"]
+  s.files = ["bin/whichr", "lib/whichr.rb", "README"]
   s.homepage = %q{http://github.com/rogerdpack/which_ruby}
   s.executable = 'whichr'
-
+  s.require_paths = ["lib"]
   s.add_dependency(%q<rdoc>, [">= 2.3.0"]) # so that I don't need a lib directory :P
   s.add_dependency(%q<rogerdpack-sane>) # Object.in?
      
@@ -19,3 +19,4 @@ end
 # 0.1.1 bump README
 # 0.1.2 respect ENV['PATHEXT'] and '.' on windows
 # 0.1.3 use the right capitalization in doze
+# 0.1.4 split into a re-usable lib
